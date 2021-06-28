fc580acf874e416c88b1bc4c5e497ebe4f3c561f
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-06-28 07:59:06 +0530

    Add-perform crud operations using json-server and maintain local list too

--- src/test/java/com/magic/addressbook/AddressBookRestIOTest.java
+++ src/test/java/com/magic/addressbook/AddressBookRestIOTest.java
@@ -1,8 +1,14 @@
 package com.magic.addressbook;
 
+import com.google.gson.Gson;
 import com.magic.addressbook.entity.Contact;
+import com.magic.addressbook.service.AddressBookRestIOService;
 import com.magic.addressbook.service.AddressBookServiceDB;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -12,6 +18,110 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AddressBookRestIOTest {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3000;
+    }
+
+    public Contact[] getContactList() {
+        Response response = RestAssured.get("/person");
+        System.out.println(response.asString());
+        return new Gson().fromJson(response.asString(), Contact[].class);
+    }
+
+    public Response addContactToJsonServer(Contact contact) {
+        String empJson = new Gson().toJson(contact);
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(empJson);
+        return request.post("/person");
+    }
+
+    @Test
+    public void givenContactDataInJsonServer_WhenRetrieved_ShouldMatchTheCount() {
+        Contact[] contacts = getContactList();
+        AddressBookRestIOService addressBookRestIOService;
+        addressBookRestIOService = new AddressBookRestIOService(Arrays.asList(contacts));
+        addressBookRestIOService.printList();
+        long entries = addressBookRestIOService.countEntries();
+        Assertions.assertEquals(2, entries);
+    }
+
+    @Test
+    public void givenNewContact_WhenAdded_ShouldMatch201ResponseAndCount() {
+        Contact[] contacts = getContactList();
+        AddressBookRestIOService addressBookRestIOService;
+        addressBookRestIOService = new AddressBookRestIOService(Arrays.asList(contacts));
+        Contact contact = new Contact(0, "Edward", "Cullan", "New York", "USA", "11091",
+                "7838288097", "edward@gmail.com");
+        Response response = addContactToJsonServer(contact);
+        int statusCode = response.statusCode();
+        Assertions.assertEquals(201, statusCode);
+
+        contact = new Gson().fromJson(response.asString(), Contact.class);
+        addressBookRestIOService.addEmployeeToList(contact);
+        long entries = addressBookRestIOService.countEntries();
+        Assertions.assertEquals(2, entries);
+
+    }
+
+    @Test
+    public void givenNewState_WhenUpdated_ShouldMatch200Response() {
+        Contact[] contacts = getContactList();
+        AddressBookRestIOService addressBookRestIOService;
+        addressBookRestIOService = new AddressBookRestIOService(Arrays.asList(contacts));
+        addressBookRestIOService.updateStateInContactList("Mike", "Canada");
+        Contact contact = addressBookRestIOService.getContact("Mike");
+
+        String empJson = new Gson().toJson(contact);
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(empJson);
+        Response response = request.put("/person/" + contact.id);
+        int statusCode = response.getStatusCode();
+        Assertions.assertEquals(200, statusCode);
+    }
+
+    @Test
+    public void givenMultipleEmployee_WhenAdded_ShouldMatch201ResponseAndCount() {
+        Contact[] contacts = getContactList();
+        AddressBookRestIOService addressBookRestIOService;
+        addressBookRestIOService = new AddressBookRestIOService(Arrays.asList(contacts));
+        Contact[] contacts1 = {
+                new Contact(0, "Mike", "Smith", "NEw Jersey", "USA", "1109", "9891982098", "mike@gmail.com"),
+                new Contact(0, "Bella", "Cavil", "Los Angelos", "USA", "11009", "878984", "bella@gmail.com")
+        };
+        Arrays.stream(contacts1).forEach(contact -> {
+            Response response = addContactToJsonServer(contact);
+            int statusCode = response.getStatusCode();
+            Assertions.assertEquals(201, statusCode);
+
+            contact = new Gson().fromJson(response.asString(), Contact.class);
+            addressBookRestIOService.addEmployeeToList(contact);
+        });
+        long entries = addressBookRestIOService.countEntries();
+        Assertions.assertEquals(5, entries);
+    }
+
+    @Test
+    public void givenEmployeeToDelete_WhenDeleted_ShouldMatch200ResponseAndCount() {
+        Contact[] contacts = getContactList();
+        AddressBookRestIOService addressBookRestIOService;
+        addressBookRestIOService = new AddressBookRestIOService(Arrays.asList(contacts));
+
+        Contact contact = addressBookRestIOService.getContact("Mike");
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        Response response = request.delete("/person/" + contact.id);
+        int statusCode = response.getStatusCode();
+        Assertions.assertEquals(200, statusCode);
+
+        addressBookRestIOService.deleteEmployee(contact.firstName);
+        long entries = addressBookRestIOService.countEntries();
+        Assertions.assertEquals(1, entries);
+    }
 
     @Test
     public void givenPersonInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws SQLException {
